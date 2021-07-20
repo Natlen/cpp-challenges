2d9ea10f9fc7f9aeafbea310c97447476f5ed7e6
Nativ Lengel <Nativ.Lengel@wdc.com>
2021-07-20 15:32:24 +0300

    solved

--- word-break-1/main.cpp
+++ word-break-1/main.cpp
@@ -7,30 +7,33 @@ using namespace std;
 bool word_break(string A, set<string> B, int n) {
 
     cout << "-- word-break-1 --" << endl;
-
+    int index = -1;
     set<string> segmented_words;
     for(string s : B)
     {
         if(A.empty())
-            return 1;
-        if(!A.find(s))
+            break;
+        if( (index = A.find(s)) != -1)
         {
             segmented_words.insert(s);
-            A.erase(0, s.length());
+            A.erase(index, s.length());
         }
     }
     for(string s : segmented_words)
         cout << s << " ";
     cout << endl;
+    if(A.empty())
+        return 1;
     return 0;
 }
 
 int main() {
     int n = 12;
-    set<string> B = { "i", "like", "sam", "sung", "samsung", "mobile", "ice","cream", "icecream", "man", "go", "mango" };
+    set<string> B = {"i", "like", "sam", "sung", "samsung", "mobile", "ice","cream", "icecream", "man", "go", "mango" };
     string A = "ilike";
-    word_break(A, B, n) ? cout << true : cout << false;
-
+    string A2 = "ilikesamsung";
+    word_break(A, B, n) ? cout << true << endl : cout << false << endl;
+    word_break(A2, B, n) ? cout << true << endl : cout << false << endl;
 
     system("PAUSE");
     return 0;
